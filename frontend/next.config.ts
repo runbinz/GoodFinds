95e1f11375811c27f5a9e7d1b3773723b136084d
Runbin Zhou <runbin1000@gmail.com>
2025-11-26 00:08:41 -0500

    updated config.ts for Image

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.example.com', // Allows all subdomains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
